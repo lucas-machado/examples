8d73b4c9607843abc0cc62a45a33ea52a7dd54e4
Lucas Machado <lucasmach@gmail.com>
2026-02-21 20:42:13 -0300

    Refactor TodoForm to simplify optimistic updates and error handling. Remove unnecessary context management and streamline the onError function to filter out failed todos from the query data.

--- front/mosh-tutorials/react-course-part2-starter/src/react-query/TodoForm.tsx
+++ front/mosh-tutorials/react-course-part2-starter/src/react-query/TodoForm.tsx
@@ -3,29 +3,21 @@ import { useRef } from "react";
 import { Todo } from "../hooks/useTodos";
 import axios from "axios";
 
-interface AddTodoContext {
-  previousTodos: Todo[];
-}
-
 const TodoForm = () => {
   const ref = useRef<HTMLInputElement>(null);
   const queryClient = useQueryClient();
-  const addTodo = useMutation<Todo, Error, Todo, AddTodoContext>({
+  const addTodo = useMutation<Todo, Error, Todo>({
     mutationFn: (todo: Todo) =>
       axios
         .post<Todo>("https://jsonplaceholder.typicode.com/todosx", todo)
         .then((res) => res.data),
     onMutate: (newTodo: Todo) => {
-      const previousTodos = queryClient.getQueryData<Todo[]>(["todos"]) || [];
-
       queryClient.setQueryData<Todo[]>(["todos"], (todos) => [
         newTodo,
         ...(todos || []),
       ]);
 
       if (ref.current) ref.current.value = "";
-
-      return { previousTodos };
     },
     onSuccess: (savedTodo, newTodo) => {
       queryClient.setQueryData<Todo[]>(["todos"], (todos) =>
@@ -35,7 +27,10 @@ const TodoForm = () => {
     onError: (error, newTodo, context) => {
       if (!context) return;
 
-      queryClient.setQueryData<Todo[]>(["todos"], context.previousTodos);
+      queryClient.setQueryData<Todo[]>(
+        ["todos"],
+        (current) => current?.filter((todo) => todo !== newTodo) ?? [],
+      );
     },
   });
 
