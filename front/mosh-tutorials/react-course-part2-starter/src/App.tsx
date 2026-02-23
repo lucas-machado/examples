ffd749eda1789d1dfd3e755a8da8cb3704fbfe91
Lucas Machado <lucasmach@gmail.com>
2026-02-23 09:46:34 -0300

    Remove obsolete AuthProvider import from App component following recent refactor of authentication management. This change streamlines the codebase and reflects the updated state management structure.

--- front/mosh-tutorials/react-course-part2-starter/src/App.tsx
+++ front/mosh-tutorials/react-course-part2-starter/src/App.tsx
@@ -1,7 +1,6 @@
 import "./App.css";
 import NavBar from "./state-management/NavBar";
 import HomePage from "./state-management/HomePage";
-import AuthProvider from "./state-management/auth/AuthProvider";
 import TaskProvider from "./state-management/tasks/TaskProvider";
 import Counter from "./state-management/counter/Counter";
 
