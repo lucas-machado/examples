ebdd221702e29efc0793701db06bdbffd5e917fc
Lucas Machado <lucasmach@gmail.com>
2026-02-23 16:57:38 -0300

    Add error handling and private route management
    - Implement ErrorPage component to display error messages based on route errors.
    - Introduce PrivateRoutes component to protect certain routes and redirect unauthenticated users to the login page.
    - Update routing structure to include ErrorPage and LoginPage, enhancing user experience and navigation flow.
    - Modify useAuth hook to return null for unauthenticated users, aligning with the new routing logic.

--- front/mosh-tutorials/react-course-part2-starter/src/routing/routes.tsx
+++ front/mosh-tutorials/react-course-part2-starter/src/routing/routes.tsx
@@ -4,19 +4,29 @@ import ContactPage from "./ContactPage";
 import UserDetail from "./UserDetail";
 import Layout from "./Layout";
 import UsersPage from "./UsersPage";
+import ErrorPage from "./ErrorPage";
+import LoginPage from "./LoginPage";
+import PrivateRoutes from "./PrivateRoutes";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
+    errorElement: <ErrorPage />,
     children: [
       { index: true, element: <HomePage /> },
-      {
-        path: "users",
-        element: <UsersPage />,
-        children: [{ path: ":id", element: <UserDetail /> }],
-      },
+      { path: "login", element: <LoginPage /> },
       { path: "contact", element: <ContactPage /> },
+      {
+        element: <PrivateRoutes />,
+        children: [
+          {
+            path: "users",
+            element: <UsersPage />,
+            children: [{ path: ":id", element: <UserDetail /> }],
+          },
+        ],
+      },
     ],
   },
 ]);
